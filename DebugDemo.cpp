1bf8d2081e0f2c7400041906ad6b498a925a3c7c
PMARINA <bluerat33@gmail.com>
2020-07-10 20:22:05 -0400

    first commit

---
+++ DebugDemo.cpp
@@ -0,0 +1,18 @@
+// How do you import the I/O library?
+//#include <>
+#include <cstdint>
+#include <string>
+// Set the namespace... or don't, up to you
+
+const std::string boundary =
+    "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
+
+int main() {
+  std::cout << "Part 1:" << boundary << std::endl;
+  // Write a for loop that prints out the numbers from 0 to 4, inclusive
+
+  std::cout << std::endl << "Part 2:" << boundary << std::endl;
+  // Write a for loop that prints out the numbers from 10 down to 5, inclusive
+
+  std::cout << std::endl;
+}
